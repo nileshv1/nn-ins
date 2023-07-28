f2f90c745142a6e3a9071ee2ba654d60805d5420
Damini Sharma <1000064595@hexaware.com>
2023-07-28 16:19:25 +0530

    add internationalization and fix other css issues

--- pages/simulation/Simulation.tsx
+++ pages/simulation/Simulation.tsx
@@ -7,18 +7,14 @@ import Typography from "@mui/material/Typography";
 import { Box, Grid, InputLabel, TextField } from "@mui/material";
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 import CustomModal from "@/components/Modal/CustomModal";
-import classes from "../../styles/page.module.scss";
-import {
-  SIMULATION_HEADER_TEXT,
-  SIMULATION_CONTENT_LABEL,
-  SIMULATION_NEXT_BTN,
-  SIMULATION_WITHOUT_CHASSIS_BTN,
-} from "@/constants/text_constants";
 import { ROUTE_PATHS } from "@/constants/constants";
 import { useRouter } from "next/router";
+import classes from "../../styles/page.module.scss";
+import { useTranslation } from "next-i18next";
 
 const Simulation: React.FC = () => {
   const router = useRouter();
+  const { t } = useTranslation();
   const [userInput, setUserInput] = React.useState("");
   const [open, setOpen] = React.useState(false);
 
@@ -29,6 +25,10 @@ const Simulation: React.FC = () => {
   const handleClickNext = () => {
     router.push(ROUTE_PATHS.CARDETAILS);
     setUserInput("");
+  };
+
+  const handleClick = () => {
+    router.push(ROUTE_PATHS.YOURCAR);
   };
 
   return (
@@ -48,9 +48,9 @@ const Simulation: React.FC = () => {
             />
             <CardContent>
               <Typography variant="h5" color="text.secondary">
-                {SIMULATION_HEADER_TEXT}
+                {t("simulationHeaderText")}
               </Typography>
-              {/* <hr className="horizontal-line" /> */}
+              <hr className={classes.horizontal_line} />
             </CardContent>
           </Card>
         </Grid>
@@ -58,7 +58,7 @@ const Simulation: React.FC = () => {
       <Grid container spacing={2} sx={{ px: 2 }}>
         <Grid item xs={12}>
           <InputLabel htmlFor="component-simple">
-            {SIMULATION_CONTENT_LABEL}
+            {t("simulationContentLabel")}
             <InfoOutlinedIcon onClick={() => setOpen((open) => !open)} />
             {open && <CustomModal open={open} setOpen={setOpen} />}
           </InputLabel>
@@ -75,23 +75,34 @@ const Simulation: React.FC = () => {
         <Grid item xs={12} display="flex" justifyContent="flex-end">
           <Button
             variant="contained"
-            // color="success"
-            sx={{ px: 9, backgroundColor: "#55B862" }}
+            sx={{
+              px: 9,
+              backgroundColor: "#55B862",
+              ":hover": {
+                bgcolor: "#55B862",
+              },
+            }}
             disabled={!userInput}
             onClick={handleClickNext}
           >
-            {SIMULATION_NEXT_BTN}
+            {t("simulationNextBtn")}
           </Button>
           .
         </Grid>
 
         <Grid item xs={12} display="flex" justifyContent="flex-end">
           <Button
-            href={ROUTE_PATHS.YOURCAR}
+            onClick={handleClick}
             variant="outlined"
-            className={classes.simulation_Btn}
+            sx={{
+              borderColor: "#55B862",
+              color: "#20ea3c",
+              ":hover": {
+                borderColor: "#55B862",
+              },
+            }}
           >
-            {SIMULATION_WITHOUT_CHASSIS_BTN}
+            {t("simulationWithoutChassisBtn")}
           </Button>
         </Grid>
       </Grid>
