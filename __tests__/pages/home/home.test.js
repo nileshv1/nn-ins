49512f33434dc8b258206b2aee519b6707f44d85
B Vinothini <2000093505@hexaware.com>
2023-07-28 18:29:35 +0530

    Added test cases for home page

--- __tests__/pages/home/home.test.js
+++ __tests__/pages/home/home.test.js
@@ -1,15 +1,27 @@
-import { render, screen } from '@testing-library/react'
-import Home from '../../../pages/home/index.page'
-import '@testing-library/jest-dom'
- 
-describe('Home', () => {
-  it('renders a heading', () => {
-    render(<Home />)
- 
-    const heading = screen.getByRole('heading', {
-      name: /hello/,
-    })
- 
-    expect(heading).toBeInTheDocument()
-  })
-})
+import { render, screen } from "@testing-library/react";
+import Home from "../../../pages/home";
+import "@testing-library/jest-dom";
+
+jest.mock("next/router", () => ({
+  useRouter() {
+    return {
+      route: "/",
+      pathname: "",
+      query: "",
+      asPath: "",
+    };
+  },
+}));
+
+describe("HomeTest", () => {
+  it("renders a heading", () => {
+    render(<Home />);
+    const assuranceimg = screen.getByAltText("assuranceimg");
+    expect(assuranceimg.src !== "").toBeTruthy();
+    const habitationimg = screen.getByAltText("habitationimg");
+    expect(habitationimg.src !== "").toBeTruthy();
+    expect(screen.getByText("headerInsurance")).toHaveTextContent(
+      "headerInsurance"
+    );
+  });
+});
